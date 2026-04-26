811d8e903851b37d30bcce9bdf46eeaa7dc788f6
Enrique Rivera <juane222333@gmail.com>
2026-04-25 22:23:42 -0400

    agregando SQS y eventos

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -7,12 +7,26 @@ import * as events from "aws-cdk-lib/aws-events";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as lambdaNodejs from "aws-cdk-lib/aws-lambda-nodejs";
 import * as apigateway from "aws-cdk-lib/aws-apigateway";
+import * as sqs from "aws-cdk-lib/aws-sqs";
+import * as targets from "aws-cdk-lib/aws-events-targets";
+import * as lambdaEventSources from "aws-cdk-lib/aws-lambda-event-sources";
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     const eventBus = new events.EventBus(this, "AirbnbEventBus");
+
+    const notificationQueue = new sqs.Queue(this, "NotificationQueue");
+
+    new events.Rule(this, "UserCreatedRule", {
+      eventBus,
+      eventPattern: {
+        source: ["user.service"],
+        detailType: ["user.created"]
+      },
+      targets: [new targets.SqsQueue(notificationQueue)]
+    });
 
     // Cognito User Pool
     const userPool = new cognito.UserPool(this, "UserPool", {
@@ -61,6 +75,21 @@ export class CdkStack extends cdk.Stack {
       }
     });
 
+    const notificationLambda = new lambdaNodejs.NodejsFunction(this, "NotificationLambda", {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      entry: path.join(
+        servicesRoot,
+        "services/notification-service/src/handler.ts"
+      ),
+      handler: "handleUserCreated",
+      projectRoot: servicesRoot,
+      depsLockFilePath: path.join(servicesRoot, "package-lock.json")
+    });
+
+    notificationLambda.addEventSource(
+      new lambdaEventSources.SqsEventSource(notificationQueue)
+    );
+
     // Permisos
     usersTable.grantWriteData(userLambda);
     eventBus.grantPutEventsTo(userLambda);
@@ -97,6 +126,18 @@ export class CdkStack extends cdk.Stack {
 
     new cdk.CfnOutput(this, "ApiUrl", {
       value: api.url
+    });
+
+    new cdk.CfnOutput(this, "EventBusName", {
+      value: eventBus.eventBusName
+    });
+
+    new cdk.CfnOutput(this, "NotificationQueueUrl", {
+      value: notificationQueue.queueUrl
+    });
+
+    new cdk.CfnOutput(this, "NotificationQueueName", {
+      value: notificationQueue.queueName
     });
   }
 }
